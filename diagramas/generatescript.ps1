b6c70d5a164a1fb036c9c9f601fc0f1e69f31bfe
Gustavo Maggi <gmaggiw@gmail.com>
2018-08-12 09:31:49 -0500

    Separando status en tablas

--- diagramas/generatescript.ps1
+++ diagramas/generatescript.ps1
@@ -1,3 +1,10 @@
+function TruncateId($name)
+{
+    $palabras = $name.Split('_id')
+    $palabras = $palabras[0..($palabras.length - 2)]
+
+    $palabras -join '_'
+}
 
 Remove-Item "PignusAppScaffold.sh" -Force -ErrorAction SilentlyContinue
 
@@ -7,6 +14,8 @@ $Entidades = $Json.ownedElements[1].ownedElements.name
 
 $Entidades = $Entidades[1..($Entidades.length - 1)]
 
+$comandos = @()
+
 foreach($Entidad in $Entidades)
 {
     $Columnas = $($Json.ownedElements[1].ownedElements | Where-Object { $_.name -eq $Entidad }).columns
@@ -15,9 +24,42 @@ foreach($Entidad in $Entidades)
 
     foreach($Columna in $Columnas)
     {
-        $Atributos += @("$($Columna.name):$($Columna.type)")
+        if($Columna.type -eq "VARCHAR")
+        {
+            $Atributo = "$($Columna.name):string"
+        }
+        elseif ($Columna.name.Endswith('_id')) {
+            $Atributo = "$(TruncateId($Columna.name)):references"
+        }
+        elseif ($Columna.name -eq "id") {
+            continue
+        }
+        else
+        {
+            if($Columna.type)
+            {
+                $Atributo = "$($Columna.name):$($Columna.type.ToLower())"
+            }
+            else {
+                $Atributo = "$($Columna.name):string"
+            }
+
+        }
+
+        if($Columna.unique){
+            $Atributo = $Atributo + ":uniq"
+        }
+
+        $Atributos += $Atributo
     }
 
-    Add-Content "PignusAppScaffold.sh" -Force -Value "rails g scaffold $Entidad $($Atributos -join ' ')"
+
+    $comandos += "rails g scaffold $Entidad $($Atributos -join ' ')"
 
 }
+
+$comandosWithoutReferences = $comandos | Where-Object {$_ -notlike "*references*"}
+
+$comandosWithReferences = $comandos | Where-Object {$_ -like "*references*"}
+
+Add-Content -Path "PignusAppScaffold.sh" -Value $comandosWithoutReferences -Force
